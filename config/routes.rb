cbeadd9607d9495b9489ed95e6ee0c9eb0d1c5a7
Swetha Kumar <swethakumar30@gmail.com>
2015-10-12 10:34:27 +0000

    added pagination

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
  root 'pages#home'
  get '/home' , to: 'pages#home'
  
- resources :recipes
+ resources :recipes do
+  member do 
+   post 'like'
+  
+  end
+  
+ end
  
+
 
 end
